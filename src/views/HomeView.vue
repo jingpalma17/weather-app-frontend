3b6a70bae6852e926c19f36978f1f796d2b9ad03
Jing Palma <jingpalma17@gmail.com>
2022-05-28 18:17:07 +0800

    Improved home content and added initial function for displaying the result

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,57 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h3>My Name</h3>
+    <div v-if="!state.displayResult">
+      <input name="city" type="text" placeholder="city" />
+      <button @click="submit">Display Weather</button>
+    </div>
+    <div v-else>
+      <table>
+        <thead>
+          <tr>
+            <th>Date (mm/dd/yyyy)</th>
+            <th>Temp(F)</th>
+            <th>Description</th>
+            <th>Main</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+         <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+          </tr>
+         </tbody>
+      </table>
+      <button @click="goBack">Back</button>
+    </div>
+  </div>
 </template>
+<script lang="ts">
+import { reactive } from "vue";
+
+export default {
+  name: "HomeView",
+  setup() {
+    const state: any = reactive({
+      displayResult: false
+    }); // TODO Fix typings
+
+    const submit = async () => {
+      state.displayResult = true;
+    };
+
+    const goBack = () => {
+      state.displayResult = false;
+    };
+
+    return { state, submit, goBack };
+  },
+};
+</script>
+<style></style>
