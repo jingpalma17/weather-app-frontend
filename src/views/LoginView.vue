1a642886393118c656c82cc53218b3eb8daf9f62
Jing Palma <jingpalma17@gmail.com>
2022-05-29 04:52:21 +0800

    Improved UI for the login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
+  <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6">
+    <div>
+      Welcome to the weather forecast web application. Please login with your
+      Github user to use the application and view the weather in your city.
+    </div>
+    <div class="inline-flex rounded-md shadow">
+      <button
+        @click="login"
+        class="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-blue-500 focus:outline-none"
+      >
+        Login
+      </button>
+    </div>
+  </div>
+  <!--
   <main>
     <div>
       Welcome to the weather forecast web application. Please login with your
@@ -8,6 +23,7 @@
       <button @click="login">Login</button>
     </div>
   </main>
+  -->
 </template>
 <script>
 import { useAuth0 } from "@auth0/auth0-vue";
@@ -25,5 +41,3 @@ export default {
   },
 };
 </script>
-
-
