3f985a202e5a3904bff2cd881276f2196fef0048
Jing Palma <jingpalma17@gmail.com>
2022-05-28 18:18:06 +0800

    Fixed content structure

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,11 @@ import HelloWorld from '@/components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h3>Weather Forecast</h3>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <a>Logout</a>
       </nav>
     </div>
   </header>
