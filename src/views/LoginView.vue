0eba1e4b534dc81c2eb2bbd985bf2d6f0c6d5682
Jing Palma <jingpalma17@gmail.com>
2022-05-28 18:15:49 +0800

    Implemented initial login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div>
+      Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city.
+      <button>Login</button>
+    </div>
+  
+  </main>
+</template>
