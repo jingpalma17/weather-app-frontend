5be34d073eeae190f8b4e0ac5fd831d85d836baf
Jing Palma <jingpalma17@gmail.com>
2022-05-29 11:06:18 +0800

    Added some initial validation for checking city field

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,15 @@
   <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 flex justify-center">
     <div v-if="!state.displayResult" class="flex flex-col items-center">
       <h3>{{ user.name }}</h3>
-      <a :href="'https://github.com/'+user.nickname">https://github.com/{{user.nickname}}</a>
+      <a :href="'https://github.com/' + user.nickname"
+        >https://github.com/{{ user.nickname }}</a
+      >
       <input
         name="city"
         type="text"
         class="peer border border-gray-200 focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-12"
         placeholder="City"
-        v-model="state.city" 
+        v-model="state.city"
       />
       <button
         @click="submit(state.city)"
@@ -22,22 +24,50 @@
       <table class="table-auto w-full divide-y divide-gray-200">
         <thead class="bg-gray-100">
           <tr class="border-gray-400">
-            <th class="border-2 text-left font-medium uppercase tracking-wider">Date (mm/dd/yyyy)</th>
-            <th class="border-2 text-left font-medium uppercase tracking-wider">Temp(F)</th>
-            <th class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider">Description</th>
-            <th class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider">Main</th>
-            <th class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider">Pressure</th>
-            <th class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider">Humidity</th>
+            <th class="border-2 text-left font-medium uppercase tracking-wider">
+              Date (mm/dd/yyyy)
+            </th>
+            <th class="border-2 text-left font-medium uppercase tracking-wider">
+              Temp(F)
+            </th>
+            <th
+              class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider"
+            >
+              Description
+            </th>
+            <th
+              class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider"
+            >
+              Main
+            </th>
+            <th
+              class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider"
+            >
+              Pressure
+            </th>
+            <th
+              class="hidden md:table-cell border-2 text-left font-medium uppercase tracking-wider"
+            >
+              Humidity
+            </th>
           </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
           <tr class="border-gray-400">
             <td class="border-2">{{ state.dateToday }}</td>
             <td class="border-2">{{ state.weather.main.temp }}</td>
-            <td class="hidden md:table-cell border-2">{{ state.weather.weather[0].description }}</td>
-            <td class="hidden md:table-cell border-2">{{ state.weather.weather[0].main }}</td>
-            <td class="hidden md:table-cell border-2">{{ state.weather.main.pressure }}</td>
-            <td class="hidden md:table-cell border-2">{{ state.weather.main.humidity }}</td>
+            <td class="hidden md:table-cell border-2">
+              {{ state.weather.weather[0].description }}
+            </td>
+            <td class="hidden md:table-cell border-2">
+              {{ state.weather.weather[0].main }}
+            </td>
+            <td class="hidden md:table-cell border-2">
+              {{ state.weather.main.pressure }}
+            </td>
+            <td class="hidden md:table-cell border-2">
+              {{ state.weather.main.humidity }}
+            </td>
           </tr>
         </tbody>
       </table>
@@ -72,9 +102,19 @@ export default {
     });
 
     const submit = async (city) => {
+      if (!city) {
+        alert("Please enter city");
+        return;
+      }
       const token = await getAccessTokenSilently();
       userStore.setToken(token);
-      await weatherStore.loadWeather(city);
+      try {
+        await weatherStore.loadWeather(city);
+      } catch (err) {
+        alert("Something went wrong. Please try again");
+        return;
+      }
+
       state.displayResult = true;
     };
 
