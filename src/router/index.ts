c7ef6c9538d3b667c74adbfaf930692cbb3fdd5e
Jing Palma <jingpalma17@gmail.com>
2022-05-29 08:24:06 +0800

    Fixed routing to add auth guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,33 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, RouterView } from "vue-router";
 import LoginView from "../views/LoginView.vue";
 import HomeView from "../views/HomeView.vue";
+import { authGuard } from "@auth0/auth0-vue";
+import NotFoundPage from "../views/NotFoundPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/login",
-      name: "login",
-      component: LoginView,
+      path: "/",
+      name: "root",
+      component: RouterView,
+      children: [
+        {
+          path: "/dashboard",
+          name: "home",
+          component: () => import('@/views/HomeView.vue'),
+        },
+      ],
+      beforeEnter: authGuard,
     },
     {
-      path: "/dashboard",
-      name: "home",
-      component: HomeView,
+      path: "/login",
+      name: "login",
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)',
+      component: () => import('@/views/NotFoundPage.vue'),
     },
   ],
 });
