024530f50ff2c2944d598239b1533d84a3138c1f
Jing Palma <jingpalma17@gmail.com>
2022-05-29 08:23:51 +0800

    Implemented notfoundpage

---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6">
+    <div>PAGE NOT FOUND</div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "NotFoundPage",
+};
+</script>
